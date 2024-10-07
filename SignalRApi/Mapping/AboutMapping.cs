54c624cea5219830902bad28676b0798f3961106
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2024-10-08 01:59:37 +0300

    Added new feature in the part of api

---
+++ SignalRApi/Mapping/AboutMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.AboutDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class AboutMapping : Profile
+    {
+        public AboutMapping()
+        {
+            CreateMap<About, ResultAboutDto>().ReverseMap();//ReverseMap fonksiyonu şu anlama geliyor About hem ResultAboutDto ile eşleşebilir hem de ResultAboutDto da About ile eşleşebilir.
+            CreateMap<About, CreateAboutDto>().ReverseMap();
+            CreateMap<About, GetAboutDto>().ReverseMap();
+            CreateMap<About, UpdateAboutDto>().ReverseMap();
+        }
+    }
+}

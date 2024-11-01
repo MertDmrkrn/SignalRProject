060313c5fe6fe5547c1e69ac2141e72c9ad35b0b
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2024-11-01 11:03:03 +0300

    Added new Partials

---
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,16 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Differencing;
+using System;
+
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+    public class _LayoutSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+

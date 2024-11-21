9f787192c22acc09f7a2dc009c33f4972ea3c784
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2024-11-21 22:16:20 +0300

    Added SignalR feature in the Program.cs

---
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApi.Hubs
+{
+	public class SignalRHub : Hub //Hub burada sunucu görevi görecek. Bizim dağıtım işlemimiz hub sınıfı hangisi ise onun üzerinden sağlayacağız.
+	{
+	}
+}
--- SignalRWebUI/Program.cs
+++ SignalRWebUI/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 builder.Services.AddHttpClient();   
 builder.Services.AddControllersWithViews();
@@ -14,6 +16,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -24,5 +28,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();

153490ca224281702ad98fba18fef0647872569a
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2024-11-22 23:15:47 +0300

    Added SignalR Controller and setInterval function

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,8 +1,17 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalR.DataAccessLayer.Concrete;
 
 namespace SignalRApi.Hubs
 {
-	public class SignalRHub : Hub //Hub burada sunucu görevi görecek. Bizim dağıtım işlemimiz hub sınıfı hangisi ise onun üzerinden sağlayacağız.
-	{
-	}
+    public class SignalRHub : Hub //Hub burada sunucu görevi görecek. Bizim dağıtım işlemimiz hub sınıfı hangisi ise onun üzerinden sağlayacağız.
+    {
+        SignalRContext context = new SignalRContext();
+
+        public async Task SendCategoryCount()
+        {
+            var value = context.Categories.Count();
+            await Clients.All.SendAsync("ReceiverCategoryCount", value);
+
+        }
+    }
 }

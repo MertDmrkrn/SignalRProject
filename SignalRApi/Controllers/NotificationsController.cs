a89ef49c2bdffca532a091a43b962ff50aaa517f
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2024-12-22 23:44:02 +0300

    Added Notification features

---
+++ SignalRApi/Controllers/NotificationsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationsController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpGet]
+        public IActionResult NotificationList() 
+        {
+            return Ok(_notificationService.TGetListAll());
+        }
+
+        [HttpGet("NotificationCountByStatusFalse")]
+        public IActionResult NotificationCountWithStatusFalse()
+        {
+            return Ok(_notificationService.TNotificationCountByStatusFalse());
+        }
+
+        [HttpGet("GetAllNotificationsByFalse")]
+        public IActionResult GetAllNotificationsByFalse()
+        {
+            return Ok(_notificationService.TGetAllNotificationByFalse());
+        }
+
+    }
+}

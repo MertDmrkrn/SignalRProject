d143087b80dc711f7ffafd245fa07d6ebbe4b26c
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2024-12-24 20:24:12 +0300

    Added MessageController

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -112,5 +112,12 @@ namespace SignalRApi.Hubs
             var value = _menuTableService.TGetListAll();
             await Clients.All.SendAsync("ReceiveMenuTableStatus", value);
         }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user,message);
+
+
+        }
     }
 }

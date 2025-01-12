e219afff755f275be18f23a0b4874086a8cea421
Mert Demirkıran <mert_demirkiran41@hotmail.com>
2025-01-12 23:11:09 +0300

    Rapid Api and product images are added

---
+++ SignalRWebUI/Controllers/FoodRapidApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.RapidApiDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class FoodRapidApiController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://tasty.p.rapidapi.com/recipes/list?from=0&size=60&tags=under_30_minutes"),
+                Headers =
+    {
+        { "x-rapidapi-key", "240ae6b920mshda4163f0d03b241p1c156ajsn00f5fc3c0a23" },
+        { "x-rapidapi-host", "tasty.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                //var values = JsonConvert.DeserializeObject<List<ResultTastyApi>>(body);
+                //return View(values.ToList());
+                var root = JsonConvert.DeserializeObject<RootTastyApi>(body);
+                var values=root.Results;
+                return View(values.ToList());
+            }
+        }
+    }
+}
